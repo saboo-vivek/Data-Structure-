9790719897074dc1bac3e9db3cd2376edfe1dc53
Vivek Sahu <54233606+saboo-vivek@users.noreply.github.com>
2021-10-07 12:57:09 +0530

    Create reverse_level_order.cpp

---
+++ reverse_level_order.cpp
@@ -0,0 +1,73 @@
+// Print level order traversal in reverse order
+
+/* Binary tree - Level Order Traversal */
+#include<iostream>
+#include<queue>
+#include<stack>
+using namespace std;
+
+struct Node {
+	char data;
+	Node *left;
+	Node *right;
+};
+
+// Function to print Nodes in a binary tree in Level order
+void LevelOrder(Node *root) {
+	if(root == NULL) return;
+	stack<Node*> s;
+	queue<Node*> Q;
+	Q.push(root);  
+	s.push(root);
+	//while there is at least one discovered node
+	while(!Q.empty()) {
+		Node* current = Q.front();
+		Q.pop(); // removing the element at front
+		// cout<<current->data<<" ";
+		if(current->left != NULL) 
+			{
+				Q.push(current->left);
+				s.push(current->left);
+
+			}
+		if(current->right != NULL) 
+			{
+				Q.push(current->right);
+				s.push(current->right);
+			}
+	}
+	while(!s.empty())
+	{
+		Node* current = s.top();
+		cout<<current->data<<" ";
+		s.pop();
+	}
+}
+// Function to Insert Node in a Binary Search Tree
+Node* Insert(Node *root,char data) {
+	if(root == NULL) {
+		root = new Node();
+		root->data = data;
+		root->left = root->right = NULL;
+	}
+	else if(data <= root->data) root->left = Insert(root->left,data);
+	else root->right = Insert(root->right,data);
+	return root;
+}
+
+int main() {
+	/*Code To Test the logic
+	  Creating an example tree
+          M
+			   / \
+			  B   Q
+			 / \   \
+			A   C   Z
+    */
+	Node* root = NULL;
+	root = Insert(root,'M'); root = Insert(root,'B');
+	root = Insert(root,'Q'); root = Insert(root,'Z'); 
+	root = Insert(root,'A'); root = Insert(root,'C');
+	//Print Nodes in Level Order. 
+	LevelOrder(root);
+}
