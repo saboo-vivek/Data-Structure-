b8ed5c53df250ecf5e2bd50ce24ae227e7b885c8
Vivek Sahu <54233606+saboo-vivek@users.noreply.github.com>
2022-01-18 21:52:50 +0530

    Create shortest_path.cpp

---
+++ GRAPH THEORY/shortest_path.cpp
@@ -0,0 +1,18 @@
+// Floyd warshall algo
+
+vector<vector<int>> dis(n, vector(n,10001));
+       
+
+        for(auto e:edges)
+        {
+            dis[e[0]][e[1]]=dis[e[1]][e[0]]=e[2];
+        }
+        for(int i=0;i<n;i++)
+            dis[i][i]=0;
+        
+        for(int k=0;k<n;k++)
+            for(int i=0;i<n;i++)
+                for(int j=0;j<n;j++)
+                    dis[i][j]=min(dis[i][j],dis[i][k]+dis[k][j]);
+
+// ***********************************************************************************************************
